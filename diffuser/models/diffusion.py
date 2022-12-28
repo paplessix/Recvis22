1238cd909ccc0c9ee3a9c0a2799b60a09492374a
paplessix <pierre-adrien.plessix@mines-paristech.fr>
2022-12-28 23:51:25 +0100

    suppress render

--- diffuser/models/diffusion.py
+++ diffuser/models/diffusion.py
@@ -208,7 +208,6 @@ class GaussianDiffusion(nn.Module):
         noise = torch.randn_like(x_start)
         x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
         x_noisy = apply_conditioning(x_noisy, cond, self.action_dim)
-        print(x_noisy.shape)
         x_recon = self.model(x_noisy, cond, t)
         x_recon = apply_conditioning(x_recon, cond, self.action_dim)
 
@@ -224,7 +223,7 @@ class GaussianDiffusion(nn.Module):
     def loss(self, x, *args):
         batch_size = len(x)
         t = torch.randint(0, self.n_timesteps, (batch_size,), device=x.device).long()
-        # x = x[:,:,:self.action_dim] # <--- Train only on actions TODO
+        x = x[:,:,:self.action_dim] # <--- Train only on actions TODO
         return self.p_losses(x, *args, t)
 
     def forward(self, cond, *args, **kwargs):
